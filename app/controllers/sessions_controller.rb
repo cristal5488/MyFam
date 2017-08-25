c101e213b8fd26d27a38275befcac0df0eb6ba7e
Cristal <cristal_5488@hotmail.com>
2017-08-25 15:20:25 -0400

    logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
+    def create @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+
+      return render action: 'new' unless @user
+# logged in, hooray
+session[:user_id] = @user.id
+redirect_to notes_path
   end
 end
