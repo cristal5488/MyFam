6e2e8f7ca9ff8c3e6cb7be5c97348e463b0e79ae
Cristal <cristal_5488@hotmail.com>
2017-08-25 15:12:41 -0400

    save user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new user_params
+
+    return render action: 'new' unless @user.save
+
+    redirect_to root_path, notice: 'Created user'
+  end
+
+  private
+    def user_params
+    params.require(:user).permit(:name,:last_name,:username,:password,:password_confirmation)
+end
 end
